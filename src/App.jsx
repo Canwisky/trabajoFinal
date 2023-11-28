756119bc5ae2c03a1662a3a65d7f2bdec7816f9f
Candelaria <candelariapq@gmail.com>
2023-11-28 18:44:46 -0300

    agrego primeros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import RandomUUID from 'node: crypto';
+import TaskForm from './components/TaskForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tasks, setTasks] = useState ([]); // variable de estado con un arreglo vacio
+  // es el estado principal
+  
+  const addTask = (description) => { // crea un nuevo objeto para el arreglo de tareas
+    const newTask = {
+      id: RandomUUID(),
+      description: description,
+      isCompleted: false,
+    };
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Tareas</h1>
+      <TaskForm onAddTask={(text) => addTask(text)} />
     </>
   )
 }
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,12 @@
+function Input ({onAddTask}) {
+
+    return(
+        <form>
+            <label></label>
+            <input type="text" name="input" id="input" placeholder="describa la tarea" />
+            <input type="submit" value="crear"/>
+        </form>
+    );
+}
+
+export default input; 
