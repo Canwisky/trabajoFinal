9f47eb96f1df0ee675deca9a1c76e8319d4f1ced
Candelaria <candelariapq@gmail.com>
2023-11-29 13:43:26 -0300

    se agrega boton completado

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,30 @@ function App() {
     setTasks([...filteredTasks]); // desestructuro para no mandar un arreglo sino el contenido
   };
   
+  // funcion para cambiar el estado (el valor de isCompleted)
+
+  const handleCrossTask = (id) => {
+    const newStateCopy = tasks.map( t=>
+      t.id === id ? {...t, isCompleted: !t.isCompleted} : t  // funcion ternaria, recibe tasks desestructurado y cambia el valor booleano
+      );                                           // si coincide el id cambia el valor especifico, sino devuelve tasks inalterado
+      setTasks([...newStateCopy]);
+    };
+
+    // en isCompleted puede ir : true pero para poder cambiar ida y vuelta entre true o false usamos un negativo
+
   return (
     <>
     <h1>Tareas</h1>
-      <TaskForm onAddTask={(text) => addTask(text)} />
-      <TaskList onDeleteTask={(id) => handleDelete(id)} tasks={tasks}/>
+      <TaskForm onAddTask={(text) => addTask(text)}/> 
+      <TaskList 
+      tasks={tasks}
+      onDeleteTask={(id) => handleDelete(id)} 
+      onCrossTask={(id) => handleCrossTask(id)}/>
     </>
   );
 }
+
+// renderiza los componentes como elementos html pasandole "metodos custom" para que comunique funciones entre ellos
 
 export default App;
 
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,16 +1,19 @@
 
 
-function TaskList({ tasks, onDeleteTask }) {
+function TaskList({ tasks, onDeleteTask, onCrossTask }) {
     
     return (
         <div> lista de tareas 
             <ul>
-                {tasks.map(t => <li> {t.description} - {t.isCompleted ? "terminado " : "pendiente "} 
+                {tasks.map(t => <li> {t.description} - {t.isCompleted ? "completado " : "pendiente "} 
                 <button onClick={() => onDeleteTask(t.id)}> Borrar </button>
+                <button onClick={() => onCrossTask (t.id)}> Marcar Completado </button>
                 </li> )}
             </ul>
         </div>
     );
 } 
+
+// se llama a la funcion con un callback para poder pasarle un parametro?
 
 export default TaskList;
